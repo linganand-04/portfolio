f85f057bf5a2da82d7928cc7ddb9897204966d0c
linganand-04 <linganandb2004@gmail.com>
2026-09-21 13:10:51 +0530

    Added Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './styles/index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS;
+
+// Load Google Analytics script
+const script = document.createElement("script");
+script.async = true;
+script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+document.head.appendChild(script);
+
+// Initialize Google Analytics
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+gtag("js", new Date());
+gtag("config", GA_ID);
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/portfolio">
     <App />
-  </BrowserRouter>
-)
+  </BrowserRouter>,
+);
